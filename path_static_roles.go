4408d3a63175b0ba89a7ffb527336dd699667671
Tin Vo <tintvo08@gmail.com>
2024-10-14 14:08:00 -0700

    update changelog

--- path_static_roles.go
+++ path_static_roles.go
@@ -446,7 +446,8 @@ func (b *backend) pathStaticRoleList(ctx context.Context, req *logical.Request, 
 }
 
 func (b *backend) staticRole(ctx context.Context, s logical.Storage, roleName string) (*roleEntry, error) {
-	entry, err := s.Get(ctx, staticRolePath+roleName)
+	completeRole := staticRolePath + roleName
+	entry, err := s.Get(ctx, completeRole)
 	if err != nil {
 		return nil, err
 	}

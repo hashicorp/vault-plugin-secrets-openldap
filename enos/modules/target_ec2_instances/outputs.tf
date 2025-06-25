985064ad8c77a83642d8ac3d5968b09a4a0cc883
Hamza Shili <hamza.shili@hashicorp.com>
2025-06-25 14:56:01 -0700

    add ingress for ldap server and machine os and arch outputs

---
+++ enos/modules/target_ec2_instances/outputs.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+output "cluster_name" {
+  value = local.cluster_name
+}
+
+output "hosts" {
+  description = "The ec2 instance target hosts"
+  value       = local.hosts
+}
+
+output "plugin_arch" {
+  description = "machine architecture for GOARCH"
+  value = local.go_arch
+}
+
+output "plugin_os" {
+  description = "machine os for GOOS"
+  value = local.go_os
+}

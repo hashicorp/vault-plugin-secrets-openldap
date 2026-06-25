0a72f0a09a7a7966b68b41d8a2f1bf9b47080880
hashigator <lt.carbonell@ibm.com>
2026-06-25 09:19:20 -0400

    Copy LDAP tests to plugin repo
    - Copy 7 test files from vault-ent to blackbox/ package
    - Copy testdata directory with LDIF fixtures
    - Update package declarations from 'ldap' to 'blackbox'
    - Remove build tags to allow compilation without special flags
    - Register all 15 test functions in SystemTests array
    - Verify compilation successful
    
    Files migrated:
    - helpers.go - Test infrastructure and LDAP helpers
    - secrets_ldap_test.go - Main LDAP tests
    - secrets_ldap_root_credential_rollback_test.go
    - secrets_ldap_dynamic_audit_test.go
    - secrets_ldap_dynamic_deletion_test.go
    - secrets_ldap_dynamic_roles_test.go
    - secrets_ldap_dynamic_rollback_test.go
    - testdata/ - LDIF test fixtures

--- blackbox/blackbox_test.go
+++ blackbox/blackbox_test.go
@@ -1,3 +1,6 @@
+//go:build blackbox
+// +build blackbox
+
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: BUSL-1.1
 
@@ -14,6 +17,66 @@ var SystemTests = []SystemTest{
 	{
 		Name: "basic_smoke",
 		Fn:   TestBasicSmoke,
+	},
+	{
+		Name: "ldap_library_set_delete",
+		Fn:   TestLDAP_LibrarySetDelete,
+	},
+	{
+		Name: "ldap_library_set_read",
+		Fn:   TestLDAP_LibrarySetRead,
+	},
+	{
+		Name: "ldap_static_role_create",
+		Fn:   TestLDAP_StaticRoleCreate,
+	},
+	{
+		Name: "ldap_dynamic_role_audit_sensitive_data",
+		Fn:   TestLDAPDynamicRoleAuditSensitiveData,
+	},
+	{
+		Name: "ldap_dynamic_role_audit_trail",
+		Fn:   TestLDAPDynamicRoleAuditTrail,
+	},
+	{
+		Name: "ldap_dynamic_role_basic_operations",
+		Fn:   TestLDAPDynamicRoleBasicOperations,
+	},
+	{
+		Name: "ldap_dynamic_role_bulk_deletion",
+		Fn:   TestLDAPDynamicRoleBulkDeletion,
+	},
+	{
+		Name: "ldap_dynamic_role_deletion",
+		Fn:   TestLDAPDynamicRoleDeletion,
+	},
+	{
+		Name: "ldap_dynamic_role_deletion_with_active_credentials",
+		Fn:   TestLDAPDynamicRoleDeletionWithActiveCredentials,
+	},
+	{
+		Name: "ldap_dynamic_role_listing",
+		Fn:   TestLDAPDynamicRoleListing,
+	},
+	{
+		Name: "ldap_dynamic_role_rollback_on_creation_failure",
+		Fn:   TestLDAPDynamicRoleRollbackOnCreationFailure,
+	},
+	{
+		Name: "ldap_dynamic_role_rollback_on_deletion_failure",
+		Fn:   TestLDAPDynamicRoleRollbackOnDeletionFailure,
+	},
+	{
+		Name: "ldap_dynamic_role_validation",
+		Fn:   TestLDAPDynamicRoleValidation,
+	},
+	{
+		Name: "ldap_root_credential_rollback_workflows",
+		Fn:   TestLDAPRootCredentialRollbackWorkflows,
+	},
+	{
+		Name: "ldap_secrets_engine_comprehensive",
+		Fn:   TestLDAPSecretsEngineComprehensive,
 	},
 }
 
